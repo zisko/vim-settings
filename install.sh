70dbb6ca75b0e385fa868c49f277e3e1d07d1179
Jacob Mizraji <jmizraji@apple.com>
2017-03-06 14:49:02 -0800

    add install script

---
+++ install.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+echo "installing .vim from ${DIR}/vim to ~/.vim"
+mv ~/.vim ~/.vim.back
+ditto $DIR/vim ~/.vim
+echo "installing .vimrc from ${DIR}/vimrc to ~/.vimrc"
+mv ~/.vimrc.back
+ditto $DIR/vimrc ~/.vimrc
