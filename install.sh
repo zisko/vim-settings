3979ab57c29be17d2e75beff856999dc5fc8818b
Jacob Mizraji <jmizraji@apple.com>
2017-03-15 13:18:36 -0700

    changed

--- install.sh
+++ install.sh
@@ -12,6 +12,6 @@ echo "installing sublime prefs manifest from ${DIR}/Preferences.sublime-settings
 mv ~/Library/Application\ Support/Sublime\ Text\ 3/Packages/User/Preferences.sublime-settings ~/Library/Application\ Support/Sublime\ Text\ 3/Packages/User/Preferences.sublime-settings.back
 ditto $DIR/Preferences.sublime-settings ~/Library/Application\ Support/Sublime\ Text\ 3/Packages/User/Preferences.sublime-settings
 
-echo "installing sublime plugin manifest from ${DIR}/Package Control.sublime-settings to ~/.vimrc"
+echo "installing sublime plugin manifest from ${DIR}/Package Control.sublime-settings to ~/Library/Application\ Support/Sublime\ Text\ 3/Packages/User/Package\ Control.sublime-settings"
 mv ~/Library/Application\ Support/Sublime\ Text\ 3/Packages/User/Package\ Control.sublime-settings ~/Library/Application\ Support/Sublime\ Text\ 3/Packages/User/Package\ Control.sublime-settings.back
 ditto $DIR/Package\ Control.sublime-settings ~/Library/Application\ Support/Sublime\ Text\ 3/Packages/User/Package\ Control.sublime-settings
