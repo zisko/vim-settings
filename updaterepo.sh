33346e84218173279ec72a931b13457e2301e41a
Jacob Mizraji <jmizraji@apple.com>
2017-03-15 12:58:54 -0700

    updaterepo script

---
+++ updaterepo.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+ditto ~/.vim $DIR/vim 
+
+ditto ~/.vimrc $DIR/vimrc
+
+ditto ~/Library/Application\ Support/Sublime\ Text\ 3/Packages/User/Preferences.sublime-settings $DIR/Preferences.sublime-settings
+
+ditto ~/Library/Application\ Support/Sublime\ Text\ 3/Packages/User/Package\ Control.sublime-settings $DIR/Preferences.sublime-settings
